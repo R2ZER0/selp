158e206cff59140eb9fecd5238919bb2374e6631
Rikki Guy <git@rikkiguy.me.uk>
2014-12-13 15:37:33 +0000

    Added the generated Schema.pm and Result/*.pm modules

---
+++ lib/SquareStats/Schema/Result/Kill.pm
@@ -0,0 +1,163 @@
+use utf8;
+package SquareStats::Schema::Result::Kill;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+=head1 NAME
+
+SquareStats::Schema::Result::Kill
+
+=cut
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+=head1 TABLE: C<kill>
+
+=cut
+
+__PACKAGE__->table("kill");
+
+=head1 ACCESSORS
+
+=head2 id
+
+  data_type: 'integer'
+  is_auto_increment: 1
+  is_nullable: 0
+  sequence: 'kill_id_seq'
+
+=head2 game
+
+  data_type: 'integer'
+  is_foreign_key: 1
+  is_nullable: 0
+
+=head2 killer
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 victim
+
+  data_type: 'varchar'
+  is_nullable: 0
+  size: 255
+
+=head2 weapon
+
+  data_type: 'enum'
+  extra: {custom_type_name => "weapon_t",list => ["knife","pistol","carbine","shotgun","subgun","sniper","assault","grenade"]}
+  is_nullable: 0
+
+=head2 time
+
+  data_type: 'timestamp with time zone'
+  is_nullable: 0
+
+=head2 gib
+
+  data_type: 'boolean'
+  is_nullable: 0
+
+=head2 suicide
+
+  data_type: 'boolean'
+  is_nullable: 0
+
+=head2 killer_pos
+
+  data_type: 'vec3_t'
+  is_nullable: 0
+
+=head2 victim_pos
+
+  data_type: 'vec3_t'
+  is_nullable: 0
+
+=cut
+
+__PACKAGE__->add_columns(
+  "id",
+  {
+    data_type         => "integer",
+    is_auto_increment => 1,
+    is_nullable       => 0,
+    sequence          => "kill_id_seq",
+  },
+  "game",
+  { data_type => "integer", is_foreign_key => 1, is_nullable => 0 },
+  "killer",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "victim",
+  { data_type => "varchar", is_nullable => 0, size => 255 },
+  "weapon",
+  {
+    data_type => "enum",
+    extra => {
+      custom_type_name => "weapon_t",
+      list => [
+        "knife",
+        "pistol",
+        "carbine",
+        "shotgun",
+        "subgun",
+        "sniper",
+        "assault",
+        "grenade",
+      ],
+    },
+    is_nullable => 0,
+  },
+  "time",
+  { data_type => "timestamp with time zone", is_nullable => 0 },
+  "gib",
+  { data_type => "boolean", is_nullable => 0 },
+  "suicide",
+  { data_type => "boolean", is_nullable => 0 },
+  "killer_pos",
+  { data_type => "vec3_t", is_nullable => 0 },
+  "victim_pos",
+  { data_type => "vec3_t", is_nullable => 0 },
+);
+
+=head1 PRIMARY KEY
+
+=over 4
+
+=item * L</id>
+
+=back
+
+=cut
+
+__PACKAGE__->set_primary_key("id");
+
+=head1 RELATIONS
+
+=head2 game
+
+Type: belongs_to
+
+Related object: L<SquareStats::Schema::Result::Game>
+
+=cut
+
+__PACKAGE__->belongs_to(
+  "game",
+  "SquareStats::Schema::Result::Game",
+  { id => "game" },
+  { is_deferrable => 0, on_delete => "NO ACTION", on_update => "NO ACTION" },
+);
+
+
+# Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-12-13 14:06:06
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:+y0n3Qa6GFyLI+kgeA13iw
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
