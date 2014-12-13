2fac530185748c74ae8518a2eee2012db675a0bd
Rikki Guy <git@rikkiguy.me.uk>
2014-12-13 18:44:03 +0000

    Manager: connect to the ZMQ publisher

--- lib/SquareStats/Manager.pm
+++ lib/SquareStats/Manager.pm
@@ -1,17 +1,37 @@
 package SquareStats;
+use warnings;
+use strict;
 use Moops;
 
-class Manager {
+# ABSTRACT: the Manager listens for events from the assaultcube server, and
+#           notifies the loaded plugins of each event.
+
+class Manager with MooseX::Role::Pluggable {
+        use ZMQx::Class;
         use AnyEvent;
 
         has 'endpoint' => (
                 is => 'ro',
                 isa => 'Str',
-                required => 1
+                required => 1,
         );
+
+        has 'zmq_subscriber' => (
+                is => 'lazy',
+                isa => 'ZMQx::Class::Socket',
+        );
+
+        method _build_zmq_subscriber () {
+                return ZMQx::Class->socket('SUB', connect => $self->endpoint);
+        }
+
+        method on_recv_json () {
+               # Process the JSON into an event 
+        }
 
         method run () {
                 # Run the manager!
         }
-
 };
+
+1;
