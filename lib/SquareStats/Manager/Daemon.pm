cd30e10e18713caf3cd07f1df12bdf66d9d48824
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 19:38:26 +0000

    Fix SS::Manager::Daemon error, where MooseX::Daemonize was loaded too late

--- lib/SquareStats/Manager/Daemon.pm
+++ lib/SquareStats/Manager/Daemon.pm
@@ -3,8 +3,9 @@ use SquareStats::Declare;
 class SquareStats::Manager::Daemon
 extends SquareStats::Manager
 with MooseX::Getopt
-with MooseX::Daemonize
 {
+        with 'MooseX::Daemonize';
+
         after start() {
                 return unless $self->is_daemon;
                 $self->init();
