32053e6cdb1e9db70b67a7228cb80af450f3a1a7
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 13:20:06 +0000

    Update SquareStats::Event to use MooseX::Declare

--- lib/SquareStats/Event.pm
+++ lib/SquareStats/Event.pm
@@ -1,29 +1,27 @@
-package SquareStats::Event;
-use Moose::Role;
-use Method::Signatures;
-use MooseX::Types::JSON qw/ JSON /;
-use JSON::XS;
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
 
-has 'type' => (
-        is => 'ro',
-        isa => 'Str',
-        required => 1
-);
-
-# Serialise this event as JSON
-method to_json() {
-        # TODO
+role SquareStats::Event {
+        use MooseX::Types::JSON qw/ JSON /;
+        use JSON::XS;
+        
+        # Consumers must implement this method which returns the type
+        # that it supports.
+        requires 'type';
+        
+        # Serialise this event as JSON
+        method to_json() {
+                # TODO
+        }
+        
+        # Deserialise the json, inspect the type, create and return a
+        # relevant SquareStats::Event::* object (class method).
+        method from_json($class: JSON $json) {
+                # TODO
+        }
+        
+        # Return a list of supported event types (class method).
+        method supported_events($class:) {
+                # TODO
+        }
 }
-
-# Deserialise the json, inspect the type, create and return a relevant
-# SquareStats::Event::* object.
-func from_json(JSON $json) {
-        # TODO
-}
-
-# Return a list of supported event types
-func supported_events() {
-        # TODO
-}
-
-1;
