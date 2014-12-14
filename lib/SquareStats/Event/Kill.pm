94e41c6f2eca6ee653e5ce329683adfc0c5f1d7f
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 14:44:18 +0000

    Add SS::Event::Kill, a data object for the kill event

---
+++ lib/SquareStats/Event/Kill.pm
@@ -0,0 +1,23 @@
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
+
+
+class SquareStats::Event::Kill with SquareStats::Event {
+        use Moose::Util::TypeConstraints;
+        
+        subtype 'Natural',
+            as 'Int',
+            where { $_ > 0 };
+        
+        enum 'Weapon',
+            [qw{knife pistol carbine shotgun subgun sniper assault grenade}];
+        
+        has 'killer'  => (is => 'ro', isa => 'Str', required => 1);
+        has 'victim'  => (is => 'ro', isa => 'Str', required => 1);
+        has 'weapon'  => (is => 'ro', isa => 'Weapon', required => 1);
+        has 'time'    => (is => 'ro', isa => 'Natural', required => 1);
+        has 'gib'     => (is => 'ro', isa => 'Bool', required => 1);
+        has 'suicide' => (is => 'ro', isa => 'Bool', required => 1);        
+}
+
+1;
