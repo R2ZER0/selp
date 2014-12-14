58b28e5c0a5eb0cbd15f45a8a6e338c1489458e8
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 14:40:31 +0000

    Implemented SS::Event->from_json(...)

--- lib/SquareStats/Event.pm
+++ lib/SquareStats/Event.pm
@@ -1,13 +1,15 @@
 use MooseX::Declare;
 use Method::Signatures::Modifiers;
 
+# ABSTRACT: A generic event role, to automatically de/serialise events
 role SquareStats::Event {
         use MooseX::Types::JSON qw/ JSON /;
         use JSON::XS;
         use Module::Find;
+        use List::Util;
 
-        # Find all the available SquareStats::Event::* modules
-        our @_found_modules = usesub SquareStats::Event;
+        # Find all the available SquareStats::Event::* classes
+        our @_found_classes = usesub SquareStats::Event;
         
         # Consumers must implement this method which returns the type
         # that it supports as a string.
@@ -21,12 +23,30 @@ role SquareStats::Event {
         # Deserialise the json, inspect the type, create and return a
         # relevant SquareStats::Event::* object (class method).
         method from_json($class: JSON $json) {
-                # TODO
+                my $data = decode_json $json;
+                my $type = $data->{'type'};
+                undef $data->{'type'};
+
+                if(any { $_ eq $type } @{$class->supported_events}) {
+                        return $class->get_event_class()->new($data);
+                } else {
+                        return;
+                }
         }
         
         # Return an arrayref of supported event types (class method).
         method supported_events($class:) {
-                my @events = map { $_->type } @_found_modules;
+                my @events = map { $_->type } @_found_classes;
                 return \@events;
+        }
+
+        # Get the class assosciated with an event type
+        method get_event_class($class: Str $event) {
+                foreach my $evclass (@_found_classes) {
+                        if($evclass->type eq $event) {
+                                return $evclass;
+                        }
+                }
+                return '';
         }
 }
