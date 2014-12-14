2b98dbe440f65e3e3d1e0558637b6ab5d0f6ebe3
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 15:45:08 +0000

    Converted SS::Manager to use SS::Declare

--- lib/SquareStats/Manager.pm
+++ lib/SquareStats/Manager.pm
@@ -1,55 +1,54 @@
-package SquareStats::Manager;
-
-use namespace::autoclean;
-use Moose;
-use Method::Signatures;
-use Moose::Util::TypeConstraints;
-use MooseX::Types::JSON qw/ JSON /;
-use JSON::XS;
+use SquareStats::Declare;
 
 # ABSTRACT: the Manager listens for events from the assaultcube server, and
 #           notifies the loaded plugins of each event.
-use ZMQx::Class;
-use AnyEvent;
-with 'MooseX::Role::Pluggable';
-
-has 'endpoint' => (
-        is => 'ro',
-        isa => 'Str',
-        required => 1,
-);
-
-has 'zmq_subscriber' => (
-        is => 'ro',
-        lazy => 1,
-        builder => '_build_zmq_subscriber',
-        isa => 'ZMQx::Class::Socket',
-);
-
-method _build_zmq_subscriber() {
-        return ZMQx::Class->socket('SUB', connect => $self->endpoint);
-}
-
-method emit_event(Str $event, HashRef $data) {
-        my $method = "on_$event";
-        foreach my $plugin (@{ $self->plugin_list }) {
-                if($plugin->can($method)) {
-                        $plugin->$method($data);
+class SquareStats::Manager
+with MooseX::Role::Pluggable
+{
+        use AnyEvent;
+        use ZMQx::Class;
+        use MooseX::Types::JSON qw/ JSON /;
+        use JSON::XS;
+         
+        
+        has 'endpoint' => (
+                is => 'ro',
+                isa => 'Str',
+                required => 1,
+        );
+        
+        has 'zmq_subscriber' => (
+                is => 'ro',
+                lazy => 1,
+                builder => '_build_zmq_subscriber',
+                isa => 'ZMQx::Class::Socket',
+        );
+        
+        method _build_zmq_subscriber() {
+                return ZMQx::Class->socket('SUB', connect => $self->endpoint);
+        }
+        
+        method emit_event(Str $event, HashRef $data) {
+                my $method = "on_$event";
+                foreach my $plugin (@{ $self->plugin_list }) {
+                        if($plugin->can($method)) {
+                                $plugin->$method($data);
+                        }
                 }
         }
+        
+        # TODO: Catch exception from invalid JSON
+        method on_recv_json(JSON $json) {
+                my $data = decode_json $json;
+                $self->emit_event($data->{'type'}, $data);
+        }
+        
+        
+        method run() {
+                # Run the manager!
+        };
+        
+        
 }
 
-# TODO: Catch exception from invalid JSON
-method on_recv_json(JSON $json) {
-        my $data = decode_json $json;
-        $self->emit_event($data->{'type'}, $data);
-}
-
-
-method run() {
-        # Run the manager!
-};
-
-
-__PACKAGE__->meta->make_immutable;
 1;
