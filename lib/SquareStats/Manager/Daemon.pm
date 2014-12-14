9c5cb6eac7754cb27c1b8e2faeb6dfe96dcf8c03
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 19:29:30 +0000

    Added SS::Manager::Daemon, a forking daemon of SS::Manager, using MooseX::Daemonize

---
+++ lib/SquareStats/Manager/Daemon.pm
@@ -0,0 +1,16 @@
+use SquareStats::Declare;
+
+class SquareStats::Manager::Daemon
+extends SquareStats::Manager
+with MooseX::Getopt
+with MooseX::Daemonize
+{
+        after start() {
+                return unless $self->is_daemon;
+                $self->init();
+        }
+
+        before stop() {
+                $self->deinit();
+        }
+}
