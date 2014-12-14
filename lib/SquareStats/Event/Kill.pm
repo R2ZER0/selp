236a3ac250bbd1b293f683733fc110226b45fea0
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 14:46:32 +0000

    Implement 'type' method for SS::Event::Kill

--- lib/SquareStats/Event/Kill.pm
+++ lib/SquareStats/Event/Kill.pm
@@ -1,7 +1,6 @@
 use MooseX::Declare;
 use Method::Signatures::Modifiers;
 
-
 class SquareStats::Event::Kill with SquareStats::Event {
         use Moose::Util::TypeConstraints;
         
@@ -12,12 +11,13 @@ class SquareStats::Event::Kill with SquareStats::Event {
         enum 'Weapon',
             [qw{knife pistol carbine shotgun subgun sniper assault grenade}];
         
+        method type() { 'kill' }
         has 'killer'  => (is => 'ro', isa => 'Str', required => 1);
         has 'victim'  => (is => 'ro', isa => 'Str', required => 1);
         has 'weapon'  => (is => 'ro', isa => 'Weapon', required => 1);
         has 'time'    => (is => 'ro', isa => 'Natural', required => 1);
         has 'gib'     => (is => 'ro', isa => 'Bool', required => 1);
-        has 'suicide' => (is => 'ro', isa => 'Bool', required => 1);        
+        has 'suicide' => (is => 'ro', isa => 'Bool', required => 1);
 }
 
 1;
