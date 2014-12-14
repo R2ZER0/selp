9f71c7c61f05f967619113aae298844e9e30c701
Rikki Guy <git@rikkiguy.me.uk>
2014-12-14 10:42:01 +0000

    Switch to plain Moose instead of Moops, due to strange compile errors.

--- lib/SquareStats/Manager.pm
+++ lib/SquareStats/Manager.pm
@@ -1,37 +1,43 @@
-package SquareStats;
-use warnings;
-use strict;
-use Moops;
+package SquareStats::Manager;
+
+use namespace::autoclean;
+use Moose;
+use Method::Signatures;
+use Moose::Util::TypeConstraints;
+use MooseX::Types::JSON qw/ JSON /;
 
 # ABSTRACT: the Manager listens for events from the assaultcube server, and
 #           notifies the loaded plugins of each event.
+#
+use ZMQx::Class;
+use AnyEvent;
+with 'MooseX::Role::Pluggable';
 
-class Manager with MooseX::Role::Pluggable {
-        use ZMQx::Class;
-        use AnyEvent;
+has 'endpoint' => (
+        is => 'ro',
+        isa => 'Str',
+        required => 1,
+);
 
-        has 'endpoint' => (
-                is => 'ro',
-                isa => 'Str',
-                required => 1,
-        );
+has 'zmq_subscriber' => (
+        is => 'ro',
+        lazy => 1,
+        builder => '_build_zmq_subscriber',
+        isa => 'ZMQx::Class::Socket',
+);
 
-        has 'zmq_subscriber' => (
-                is => 'lazy',
-                isa => 'ZMQx::Class::Socket',
-        );
+method _build_zmq_subscriber() {
+        return ZMQx::Class->socket('SUB', connect => $self->endpoint);
+}
 
-        method _build_zmq_subscriber () {
-                return ZMQx::Class->socket('SUB', connect => $self->endpoint);
-        }
+method on_recv_json(JSON $json) {
+        # Process the JSON into an event 
+}
 
-        method on_recv_json () {
-               # Process the JSON into an event 
-        }
-
-        method run () {
-                # Run the manager!
-        }
+method run() {
+        # Run the manager!
 };
 
+
+__PACKAGE__->meta->make_immutable;
 1;
