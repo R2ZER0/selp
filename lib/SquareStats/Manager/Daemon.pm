659c25df6841bff868f0e9bbc4d58558d6ee7e22
Rikki Guy <git@rikkiguy.me.uk>
2014-12-15 00:58:24 +0000

    Fix options management of SS::Manager::Daemon

--- lib/SquareStats/Manager/Daemon.pm
+++ lib/SquareStats/Manager/Daemon.pm
@@ -1,17 +1,37 @@
 use SquareStats::Declare;
 
 class SquareStats::Manager::Daemon
-extends SquareStats::Manager
 with MooseX::Getopt
 {
+        use SquareStats::Manager;
+        use MooseX::Types::Path::Class;
         with 'MooseX::Daemonize';
+
+        has '_manager' => (
+                is => 'ro',
+                isa => 'SquareStats::Manager',
+                lazy => 1,
+                builder => '_build_manager',
+        );
+
+        has 'configfile' => (
+                is => 'ro',
+                isa => 'Path::Class::File',
+                default => sub { "conf/sample-manager.yaml" },
+                description => 'path to configuration file',
+        );
+                
+
+        method _build_manager() {
+                return SquareStats::Manager->new_with_config(configfile => $self->configfile);
+        }
 
         after start() {
                 return unless $self->is_daemon;
-                $self->init();
+                $self->run();
         }
 
         before stop() {
-                $self->deinit();
+                $self->finish();
         }
 }
