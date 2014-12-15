5fde97478a623b6b9f23a18280411e2e4d8b9638
Rikki Guy <git@rikkiguy.me.uk>
2014-12-15 00:59:32 +0000

    Add base manager plugin, SS::Manager::Plugin::Base, a superclass for other plugins that does nothing.

---
+++ lib/SquareStats/Manager/Plugin/Base.pm
@@ -0,0 +1,18 @@
+use SquareStats::Declare;
+
+# ABSTRACT: A base Manager::Plugin that does nothing
+class SquareStats::Manager::Plugin::Base
+with MooseX::Role::Pluggable::Plugin
+{
+        method run() {
+                # Run!
+        }
+
+        method finish() {
+                # It's over!
+        }
+
+        method on_kill(HashRef $event) {
+                # Someone has been killed!
+        }
+}
