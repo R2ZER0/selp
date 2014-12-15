71e8520a4201cf51e5a2414b21ca3431a0755aa0
Rikki Guy <git@rikkiguy.me.uk>
2014-12-15 13:59:22 +0000

    Added ABSTRACT to Collector.pm

--- lib/SquareStats/Collector.pm
+++ lib/SquareStats/Collector.pm
@@ -1,5 +1,10 @@
-use strict;
-use warnings;
-package SquareStats::Collector;
+use MooseX::Declare;
+use Method::Signatures::Modifiers;
 
+# ABSTRACT: Collects and stores game statistics from AssaultCube servers
+class SquareStats::Collector
+{
+    
+
+}
 1;
