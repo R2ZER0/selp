4815b118492d6bbac13a91cc3c786387ccc727e0
Rikki Guy <git@rikkiguy.me.uk>
2014-12-15 15:23:25 +0000

    Use an overloadable method for classname in Test::AC::Manager::Plugin::Base, to allow for reusing this test.

--- t/lib/Test/AC/Manager/Plugin/Base.pm
+++ t/lib/Test/AC/Manager/Plugin/Base.pm
@@ -5,10 +5,13 @@ class Test::AC::Mananger::Plugin::Base {
         use Test::Class::Moose;
         use Try::Tiny;
         use AC::Manager::Plugin::Base;
+        no strict 'refs';
+        
+        method tclass() {
+            return 'AC::Manager::Plugin::Base';
+        }
 
-        my $class = 'AC::Manager::Plugin::Base';
-
-        method test_constructor(...) {
+        method test_constructor($class: ...) {
                 my $plugin_harness = class 
                 with MooseX::Role::Pluggable
                 {
@@ -17,19 +20,18 @@ class Test::AC::Mananger::Plugin::Base {
                 }
         
                 my $plugin_harness_obj = $plugin_harness->new_object({
-                    plugins => [ '+AC::Manager::Plugin::Base' ],
+                    plugins => [ "+" . $class->tclass() ],
                 });
                 
-                isa_ok( $plugin_harness_obj->plugin_list->[0], $class );
+                isa_ok( $plugin_harness_obj->plugin_list->[0], $class->tclass());
         }
 
-        method test_has_run_finish_methods(...) {
-                can_ok $class, 'run';
-                can_ok $class, 'finish';
+        method test_has_run_finish_methods($class: ...) {
+                can_ok($class->tclass(), 'run', 'finish');
         }
 
-        method test_has_event_methods(...) {
-                can_ok $class, 'on_kill';
+        method test_has_event_methods($class: ...) {
+                can_ok($class->tclass(), 'on_kill');
         }
 
 }
