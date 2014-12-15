e7af0b8c04d6ba90911e99b5421f51694607ba94
Rikki Guy <git@rikkiguy.me.uk>
2014-12-15 01:21:27 +0000

    Added test for SS::Manager::Plugin::Base

---
+++ t/lib/Test/SquareStats/Manager/Plugin/Base.pm
@@ -0,0 +1,34 @@
+use SquareStats::Declare;
+
+class Test::SquareStats::Mananger::Plugin::Base {
+        use Test::Class::Moose;
+        use TryCatch;
+        use SquareStats::Manager::Plugin::Base;
+
+        my $class = 'SquareStats::Manager::Plugin::Base';
+
+        method test_constructor() {
+                my $success = 1;
+                my $obj;
+
+                try {
+                        $obj = SquareStats::Manager::Plugin::Base->new();
+                } catch {
+                        $success = 0;
+                }
+                ok($success, 'construction succeeds');
+                isa_ok $obj, $class;
+        }
+
+        method test_has_run_finish_methods() {
+                can_ok $class, 'run';
+                can_ok $class, 'finish';
+        }
+
+        method test_has_event_methods() {
+                can_ok $class, 'on_kill';
+        }
+
+}
+
+
