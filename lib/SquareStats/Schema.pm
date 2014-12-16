7b298195166641e21070a177ffabe0354953feb1
Rikki Guy <git@rikkiguy.me.uk>
2014-12-16 12:25:39 +0000

    Added better ABSTRACT to SquareStats::Schema

--- lib/SquareStats/Schema.pm
+++ lib/SquareStats/Schema.pm
@@ -15,7 +15,12 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-12-13 14:06:06
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gkC5Jbvo065TsdUL2M3MtA
 
-# Abstract for Dist::Zilla
-# ABSTRACT: The database schema for SquareStats
+# ABSTRACT: 
+# This distribution contains the mostly auto-generated DBIx::Class based schema
+# for SquareStats. All the class files are generated from the actual PostgreSQL
+# schema defined in share/sql/sqarestats-schema.sql.
+# Use databases other than PostgreSQL has not been tested nor is supported.
+
+
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
