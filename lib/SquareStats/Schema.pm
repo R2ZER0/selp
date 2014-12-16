795ccc4d78ee5cd9b8c9be3d0a656de8fff876b6
Rikki Guy <git@rikkiguy.me.uk>
2014-12-16 13:58:46 +0000

    Add  to SquareStats::Schema

--- lib/SquareStats/Schema.pm
+++ lib/SquareStats/Schema.pm
@@ -15,6 +15,8 @@ __PACKAGE__->load_namespaces;
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2014-12-13 14:06:06
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:gkC5Jbvo065TsdUL2M3MtA
 
+our $VERSION = '0.004';
+
 # ABSTRACT: 
 # This distribution contains the mostly auto-generated DBIx::Class based schema
 # for SquareStats. All the class files are generated from the actual PostgreSQL
