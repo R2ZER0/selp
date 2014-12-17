2a874497a18c98f7821c07041a5e6a5df249a8ea
Rikki Guy <git@rikkiguy.me.uk>
2014-12-17 15:08:44 +0000

    Fix: remove reference code, fix comparison of mode

--- lib/SquareStats/Schema/Result/ScoreMatrixOsok.pm
+++ lib/SquareStats/Schema/Result/ScoreMatrixOsok.pm
@@ -3,32 +3,19 @@ use strict;
 use warnings;
 use base qw/DBIx::Class::Core/;
 
-my $topten = $c->model('SS::Kill')->search({
-        -and => [
-            suicide => 'FALSE',
-            gib => 'FALSE',
-        ]
-    }, {
-        select => [ 'killer', { count => 'id' } ],
-        as => [qw/ killer kills /],
-        group_by => 'killer',
-        order_by => 'kills',
-        rows => 10,
-    });
-
 __PACKAGE__->table_class('DBIx::Class::ResultSource::View');
 __PACKAGE__->table('score_matrix_osok');
 __PACKAGE__->result_source_instance->is_virtual(1);
 __PACKAGE__->result_source_instance->view_definition(
     q[
       SELECT
-        killer AS nick,
+        killer,
         SUM(CASE WHEN NOT gib AND NOT suicide THEN 1 ELSE 0 END) AS frags,
         SUM(CASE WHEN     gib AND NOT suicide THEN 1 ELSE 0 END) AS gibs,
         SUM(CASE WHEN NOT gib AND     suicide THEN 1 ELSE 0 END) AS suicides,
         ((frags + 2*gibs) - suicides) AS total_score
       FROM kill NATURAL JOIN game
-      WHERE mode LIKE 'osok%'
+      WHERE mode = 'osok'
       GROUP BY killer      
     ]
 );
