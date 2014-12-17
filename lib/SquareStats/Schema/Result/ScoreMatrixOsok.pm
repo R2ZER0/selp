8d4e97f27e901e17de12c07630ada336e91f4d65
Rikki Guy <git@rikkiguy.me.uk>
2014-12-17 16:03:45 +0000

    Fix query in ScoreMatrixOsok view, add columns

--- lib/SquareStats/Schema/Result/ScoreMatrixOsok.pm
+++ lib/SquareStats/Schema/Result/ScoreMatrixOsok.pm
@@ -9,13 +9,40 @@ __PACKAGE__->result_source_instance->is_virtual(1);
 __PACKAGE__->result_source_instance->view_definition(
     q[
       SELECT
-        killer,
-        SUM(CASE WHEN NOT gib AND NOT suicide THEN 1 ELSE 0 END) AS frags,
-        SUM(CASE WHEN     gib AND NOT suicide THEN 1 ELSE 0 END) AS gibs,
-        SUM(CASE WHEN NOT gib AND     suicide THEN 1 ELSE 0 END) AS suicides,
-        ((frags + 2*gibs) - suicides) AS total_score
-      FROM kill NATURAL JOIN game
-      WHERE mode = 'osok'
-      GROUP BY killer      
+        kill.killer AS nick,
+        SUM(CASE WHEN NOT kill.gib AND NOT kill.suicide THEN 1 ELSE 0 END) AS frags,
+        SUM(CASE WHEN     kill.gib AND NOT kill.suicide THEN 1 ELSE 0 END) AS gibs,
+        SUM(CASE WHEN NOT kill.gib AND     kill.suicide THEN 1 ELSE 0 END) AS suicides,
+        
+        SUM(CASE WHEN NOT kill.gib AND NOT kill.suicide THEN 1 ELSE 0 END)
+        + 2*SUM(CASE WHEN kill.gib AND NOT kill.suicide THEN 1 ELSE 0 END)
+        - SUM(CASE WHEN NOT kill.gib AND kill.suicide THEN 1 ELSE 0 END)
+            AS total_score
+            
+      FROM kill, game
+      WHERE kill.game = game.id
+      AND mode = 'osok'
+      GROUP BY kill.killer
     ]
 );
+
+__PACKAGE__->add_columns(
+    'nick' => {
+      data_type => 'varchar',
+      size      => 255,
+    },
+    'frags' => {
+      data_type => 'integer',
+    },
+    'gibs' => {
+      data_type => 'integer',
+    },
+    'suicides' => {
+      data_type => 'integer',
+    },
+    'total_score' => {
+      data_type => 'integer',
+    },
+  );
+  
+1;
